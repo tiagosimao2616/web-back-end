72108041ac8aa1b1d35c4b8977bddf9b1beabc1d
tiagosimao2616 <tiagosimao2616@gmail.com>
2023-02-27 11:01:52 +0000

    Update index.js

--- Ficha1/index.js
+++ Ficha1/index.js
@@ -1,1 +1,6 @@
-console.log("Hello World");
+function calculateFinalGrade(np, nt){
+    var nf = np * 0.6 + nt * 0.4
+    return nf
+}
+var FinalGrade = calculateFinalGrade(14,12)
+console.log(FinalGrade)
