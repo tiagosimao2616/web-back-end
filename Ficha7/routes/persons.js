dcad950346b39ae4be0541ccc030d7ddb2c8424c
tiagosimao2616 <tiagosimao2616@gmail.com>
2023-05-02 20:53:48 +0100

    Update persons.js

--- Ficha7/routes/persons.js
+++ Ficha7/routes/persons.js
@@ -1,6 +1,24 @@
 var express = require('express');
 var router = express.Router();
 
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'ficha7',
+    password: '',
+    database: 'my_db'
+});
+
+connection.connect()
+
+connection.query('SELECT 1 + 1 AS solution', function (err, rows, fields) {
+    if (err) throw err
+
+    console.log('The solution is: ', rows[0].solution)
+}) 
+
+connection.end()
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
