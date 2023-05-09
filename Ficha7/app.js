0cb99425d52cac5feaac2e06b7cc351048fbc936
tiagosimao2616 <tiagosimao2616@gmail.com>
2023-05-09 20:48:02 +0100

    09/05

--- Ficha7/app.js
+++ Ficha7/app.js
@@ -4,10 +4,31 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+const swaggerJsDoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+const options = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'Hello World',
+      version: '1.0.0',
+    },
+  },
+  apis: ['./routes/*.js'], // files containing annotations as above
+};
+
+
+const swaggerDocs = swaggerJsDoc(options);
+
+
+
 var app = express();
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -27,6 +48,7 @@ app.use(function(req, res, next) {
   next(createError(404));
 });
 
+
 // error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
@@ -36,6 +58,27 @@ app.use(function(err, req, res, next) {
   // render the error page
   res.status(err.status || 500);
   res.render('error');
+});
+
+
+ /**
+* @swagger
+* /person:
+* get:
+* tags:
+* - Person
+* summary: Gets a list of persons
+* description: Returns a list of persons
+* produces:
+* - application/json
+* responses:
+* 200:
+* description: An array of persons
+* schema:
+* $ref: '#/definitions/Person'
+*/
+app.get('/', function(req, res, next) {
+  res.send('respond with a resource');
 });
 
 module.exports = app;
--- Ficha7/routes/persons.js
+++ Ficha7/routes/persons.js
@@ -1,29 +1,98 @@
 var express = require('express');
+var mysql = require('mysql')
 var router = express.Router();
 
-var mysql = require('mysql');
+//Create Connection
 var connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'ficha7',
-    password: '',
-    database: 'my_db'
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'ficha7'
 });
 
-connection.connect()
-
-connection.query('SELECT 1 + 1 AS solution', function (err, rows, fields) {
-    if (err) throw err
-
-    console.log('The solution is: ', rows[0].solution)
-}) 
-
-connection.end()
-
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+/**
+ * @openapi
+ * /:
+ *   get:
+ *     description: Welcome to swagger-jsdoc!
+ *     responses:
+ *       200:
+ *         description: Returns a mysterious string.
+ */
+router.get('/', (req, res, next) => {
+  connection.query('select * from persons', (err, results, fields) => {
+    res.send(results);
+  })
 });
 
+/**
+ * @openapi
+ * /:
+ *   get:
+ *     description: Welcome to swagger-jsdoc!
+ *     responses:
+ *       200:
+ *         description: Returns a mysterious string.
+ */
+router.get('/:id', (req, res, next) => {
+  var id = req.params.id;
+  connection.query("select * from persons where persons_id = ?", id, (err, results, fields) => {
+    res.send(results);
+  })
+});
 
+/**
+ * @openapi
+ * /:
+ *   post:
+ *     description: Welcome to swagger-jsdoc!
+ *     responses:
+ *       200:
+ *         description: Returns a mysterious string.
+ */
+router.post('/', (req, res, next) => {
+  var person = req.body;
+  connection.query("insert into persons set ?", [person], (err, results, fields) => {
+    res.send(results);
+  })
+});
+
+/**
+ * @openapi
+ * /:
+ *   delete:
+ *     description: Welcome to swagger-jsdoc!
+ *     responses:
+ *       200:
+ *         description: Returns a mysterious string.
+ */
+router.delete('/:id', (req, res, next) => {
+  var id = req.params.id;
+  connection.query("delete from persons where persons_id = ?", id, (err, results, fields) => {
+    if (results.affectedRows == 0) {
+      res.status(404).end("Id " + id + " not found")
+    }
+    else {
+      res.send("Person deleted with id: " + id)
+    }
+  })
+});
+
+/**
+ * @openapi
+ * /:
+ *   get:
+ *     description: Welcome to swagger-jsdoc!
+ *     responses:
+ *       200:
+ *         description: Returns a mysterious string.
+ */
+router.get('/:age/:profession', (req, res, next) => {
+  var age = req.params.age;
+  var profession = req.params.profession;
+  connection.query("select * from persons where age = ? and profession = ?", [age, profession], (err, results, fields) => {
+    res.send(results);
+  })
+});
 
 module.exports = router;
